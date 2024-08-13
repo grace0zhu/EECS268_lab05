94bcd4d172ca0ba140235fb64d2a43f0b2e8e6c0
grace0zhu <67290711+grace0zhu@users.noreply.github.com>
2024-08-13 13:33:25 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include "Executive.h"
 
+int main(int argc, char** argv)
+{
+    if(argc < 2) {
+        std::cout << "\nERROR: Filename needs to be passed";
+    }
+    else{
+        Executive exec(argv[1]);
+        exec.run();
+    }
+
+}
